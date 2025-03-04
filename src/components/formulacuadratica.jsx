e6d8fc3bbff1fa5e9b19cb153a3f8609c7387c67
LeoAndres10 <leo1318velasquez@hotmail.es>
2025-03-03 22:11:25 -0600

    changes

---
+++ src/components/formulacuadratica.jsx
@@ -0,0 +1,70 @@
+import UseFormula from "../hooks/UseFormula";
+
+const Formula = () => {
+
+    const {
+        valorA,
+        ValorB,
+        ValorC,
+        setValorA,
+        setValorB,
+        setValorC,
+        Resultado,
+        Resultado2,
+        Formula,
+        Limpiar
+    } = UseFormula();
+
+
+
+    return (
+
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-6 mb-3">
+                    <label className="form-label">Ingrese valor de a:</label>
+                    <input type="number" className="form-control" value={valorA} onChange={(e) => setValorA(e.target.value)} />
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <div className="col-6 mb-3">
+                    <label className="form-label">Ingrese valor de b:</label>
+                    <input type="number" className="form-control" value={ValorB} onChange={(e) => setValorB(e.target.value)} />
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <div className="col-6 mb-3">
+                    <label className="form-label">Ingrese valor de c:</label>
+                    <input type="number" className="form-control" value={ValorC} onChange={(e) => setValorC(e.target.value)} />
+                </div>
+            </div>
+            <div className="row justify-content-center">
+                <div className="col-6 mb-3">
+                    <label className="form-label">Resultado 1:</label>
+                    <input type="number" className="form-control" value={Resultado} readOnly />
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <div className="col-6 mb-3">
+                    <label className="form-label">Resultado 2:</label>
+                    <input type="number" className="form-control" value={Resultado2} readOnly />
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <div className="col-3 mb-3">
+                    <button className="btn btn-success" onClick={Formula} >Calcular</button>
+                </div>
+                <div className="col-3 mb-3">
+                    <button className="btn btn-danger" onClick={Limpiar} >Limpiar</button>
+                </div>
+            </div>
+        </div>
+    )
+    }
+
+
+export default Formula;
---
+++ src/hooks/UseFormula.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import Swal from "sweetalert2";
+
+
+const UseFormula = () => {
+
+    const [ValorA,setValorA] = useState('');
+    const [ValorB,setValorB] = useState('');
+    const [ValorC,setValorC] = useState('');
+    const [Resultado,setResultado] = useState('');
+    const [Resultado2,setResultado2] = useState('');
+const Formula = () =>  {
+
+    if (ValorA === '') {
+        Alerta('Ingrese el valor de A')
+    } else if (ValorB === '') {
+        Alerta('Ingrese el valor de B')
+       }   else if (ValorC === '') {
+            Alerta('Ingrese el valor de C')
+    } else{
+    let discriminante= Math.sqrt(Math.pow(Number(ValorB),2) - (4 * Number(ValorA) * Number(ValorC)));
+    let x1 = (-Number(ValorB) + discriminante) / (2*Number(ValorA)); 
+    let x2 = (-Number(ValorB) - discriminante) / (2*Number(ValorA));
+    setResultado(x1);
+    setResultado2(x2);
+    }
+}
+
+const Limpiar = () => {
+
+    setValorA('');
+    setValorB('');
+    setValorC('');
+    setResultado('');
+    setResultado2('');
+}
+
+const Alerta = (mensaje) => {
+    Swal.fire({
+        title: "Advertencia",
+        text: mensaje,
+        icon: "warning"
+    })
+}
+return {
+
+    ValorA,
+    setValorA,
+    ValorB,
+    setValorB,
+    ValorC,
+    setValorC,
+    Resultado,
+    Resultado2,
+    Formula,
+    Limpiar
+
+}
+    
+
+}
+
+export default UseFormula;
