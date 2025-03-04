b6fa0bbb11414085a85408c3a65626c5ccbede8e
LeoAndres10 <leo1318velasquez@hotmail.es>
2025-03-03 23:02:32 -0600

    Update UseFormula.js

--- src/hooks/UseFormula.js
+++ src/hooks/UseFormula.js
@@ -15,15 +15,12 @@ const Formula = () =>  {
         Alerta('Ingrese el valor de A')
     } else if (ValorB === '') {
         Alerta('Ingrese el valor de B')
-       }   else if (ValorC === '') {
+       } else if (ValorC === '') {
             Alerta('Ingrese el valor de C')
     }else{
-        Number(ValorA);
-        Number(ValorB);
-        Number(ValorC);
-    let discriminante = Math.sqrt(Math.pow((ValorB),2) - (4 * (ValorA) * (ValorC)));
-    let x1 = -(ValorB) + discriminante / (2*(ValorA)); 
-    let x2 = -(ValorB) - discriminante / (2*(ValorA));
+    let discriminante = Math.sqrt(Math.pow(Number(ValorB),2) - (4 * Number(ValorA) * Number(ValorC)));
+    let x1 = -Number(ValorB) + discriminante / (2*Number(ValorA)); 
+    let x2 = -Number(ValorB) - discriminante/ (2*Number(ValorA));
     setResultado(x1);
     setResultado2(x2);
     }
